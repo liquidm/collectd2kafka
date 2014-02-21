b76d73e49a85cbe02d766262f240501185f7f3fd
hflambo <hugo.flambo@liquidm.com>
2014-02-21 22:10:59 +0100

    project refactor

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mvn install:install-file \
--Dfile=kafka_2.8.0-0.8.0.jar \
+-Dfile=libs/kafka_2.8.0-0.8.0.jar \
 -DgroupId=org.apache.kafka \
 -DartifactId=kafka_2.8.0 \
 -Dversion=0.8.0-LIQUIDM \
@@ -8,7 +8,7 @@ mvn install:install-file \
 
 
 mvn install:install-file \
--Dfile=collectd-api.jar \
+-Dfile=libs/collectd-api.jar \
 -DgroupId=org.collectd \
 -DartifactId=collectd \
 -Dversion=0.0.1-LIQUIDM \
