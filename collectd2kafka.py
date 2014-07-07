564eb5c06d013808cb4d96b6cc44a672ac93bdfd
Igor Susman <igor.susman@liquidm.com>
2014-07-07 18:04:56 +0200

    add conf, kafka module doesnt read [darkstar.lqm.io:9092, localhost:9092] as suggested on https://github.com/mumrah/kafka-python/pull/122

--- collectd2kafka.py
+++ collectd2kafka.py
@@ -3,7 +3,7 @@ import os, collectd, random, json
 from kafka.client import KafkaClient
 from kafka.producer import SimpleProducer
 
-COLLECTD_PYTHON_CONF = '/etc/collectd/collectd2kafka.py.conf'
+COLLECTD_PYTHON_CONF = '/home/igor/code/collectd2kafka/collectd2kafka.py.conf'
 
 def parse_types_file(path):
     f = open(path, 'r')
@@ -37,46 +37,42 @@ def parse_types_file(path):
 
 def get_config():
     config = conf_p.ConfigParser()
-    config.read(CONFIG_FILE)
+    config.read(COLLECTD_PYTHON_CONF)
     global KAFKA_BROKERS
     global TOPIC
     global TYPES
     KAFKA_BROKERS = []
     TYPES = {}
-    for x in config.get('collectd2kafka', 'Brokers')
-        KAFKA_BROKERS.append(x)
+    KAFKA_BROKERS.append(config.get('collectd2kafka', 'Brokers'))
     TOPIC = config.get('collectd2kafka', 'Topic')
     TYPES.update(parse_types_file(config.get('collectd2kafka', 'TypesDB')))
 
-def config_callback(conf):
-    global KAFKA_BROKERS
-    global TOPIC
-    global TYPES
-    KAFKA_BROKERS = []
-    TYPES = {}
-
-
-    for node in conf.children:
-        if node.key == 'Brokers':
-            for x in node.values:
-                print x
-                KAFKA_BROKERS.append(x)
-        elif node.key == 'Topic':
-            TOPIC = node.values[0]
-        elif node.key == 'TypesDB':
-            for x in node.values:
-                TYPES.update(parse_types_file(x)) 
-
+#def config_callback(conf):
+#    global KAFKA_BROKERS
+#    global TOPIC
+#    global TYPES
+#    KAFKA_BROKERS = []
+#    TYPES = {}
+#    for node in conf.children:
+#        if node.key == 'Brokers':
+#            for x in node.values:
+#                print x
+#                KAFKA_BROKERS.append(x)
+#        elif node.key == 'Topic':
+#            TOPIC = node.values[0]
+#        elif node.key == 'TypesDB':
+#            for x in node.values:
+#                TYPES.update(parse_types_file(x)) 
 
 def write_callback(v, data=None):
     if v.type not in TYPES:
-        collectd.warning('collectd2kafka: cannot handle type %s. check types.db file?' % v.type)
+        collectd.warning('collectd2kafka: cannot handle type %s. check types.db file' % v.type)
         return
 
     v_type = TYPES[v.type]
 
     if len(v_type) != len(v.values):
-        collectd.warning('collectd2kafka: more values than type %s' % v.type)
+        collectd.warning('collectd2kafka: more values than types %s' % v.type)
         return
 
 
@@ -99,12 +95,10 @@ def write_callback(v, data=None):
         metric['values'].append(value)
 
     producer.send_messages(TOPIC, json.dumps(metric))
-print "!!!!!!!!!!!!!!!!!!!"
-print str(KAFKA_BROKERS)
-print "!!!!!!!!!!!!!!!!!!!"
 
-get_config();
-#collectd.register_config(config_callback);
-collectd.register_write(write_callback);
+get_config()
+print KAFKA_BROKERS
+#collectd.register_config(config_callback)
 kafka = KafkaClient(KAFKA_BROKERS)
 producer = SimpleProducer(kafka)
+collectd.register_write(write_callback)
